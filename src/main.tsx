40396b577be47b87d02be5888226ee4bb795a3e1
Yiyi Sun <yiyisun@gmail.com>
2021-03-28 10:49:47 -0400

    🎉 initialize apprun project

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import app from 'apprun';
+
+const state = 'Hello world - AppRun !';
+
+const view = (state) => <div>
+  <h1>{state}</h1>
+</div>;
+
+const update = {
+
+};
+
+app.start(document.body, state, view, update);
