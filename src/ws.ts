a8feddcc9e70cf3cde27f87100a19abf204271f3
Yiyi Sun <yiyisun@gmail.com>
2021-04-12 19:09:38 -0400

    add ws message queue

--- src/ws.ts
+++ src/ws.ts
@@ -1,25 +1,33 @@
 import app from 'apprun';
 
 let ws;
-function open_ws() {
+let ws_queue = [];
+
+const ws_send = msg => {
+  console.log('sending: ', msg);
+  ws.send(JSON.stringify(msg));
+}
+
+const open_ws = () => {
   ws = new WebSocket(`ws://${location.host}`);
-  ws.onopen = () => console.log('websocket connected');
+  ws.onopen = () => {
+    console.log('websocket connected');
+    ws_queue.forEach(msg => ws_send(msg));
+    ws_queue = [];
+  }
   ws.onclose = () => {
     console.log('websocket disconnected');
     ws = null;
   }
   ws.onmessage = (msg) => {
-    console.log('received: ', msg.data);
     const { event, data } = JSON.parse(msg.data);
-    app.run('@@' + event, data);
+    console.log('received: ', event, data);
+    app.run('@@' + event, data ? JSON.parse(data) : '');
   }
 }
-
-open_ws();
-
 app.on('@ws', (event, data) => {
-  if (!ws || ws.readyState === WebSocket.CLOSED) open_ws();
   const msg = { event, data };
-  console.log('sending: ', msg);
-  ws.send(JSON.stringify(msg));
+  if (!ws) open_ws();
+  if (ws.readyState === WebSocket.OPEN) ws_send(msg);
+  else ws_queue.push(msg);
 });
